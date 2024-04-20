be413108a5611cba53ed188d27de2a5d1a39c65f
jailan-sh <jilan.elsherbiny@gmail.com>
2024-04-20 14:31:19 +0200

    asnchronous

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const fs = require("fs");
 
 const path = "1-node-farm/starter/txt/";
 
-const text = fs.readFileSync(`${path}input.txt`, "utf-8");
-console.log(text);
+// blocking, synchronous:
 
-const textOut = `this new text to the old one which was avocado ${text}. \n created on ${Date.now()}.`;
-fs.writeFileSync(`${path}output.txt`, textOut);
+// const text = fs.readFileSync(`${path}input.txt`, "utf-8");
+// console.log(text);
 
-fs.appendFileSync(`${path}input.txt`, textOut, "utf-8");
+// const textOut = `this new text to the old one which was avocado ${text}. \n created on ${Date.now()}.`;
+// fs.writeFileSync(`${path}output.txt`, textOut);
 
-const newTextIn = fs.readFileSync(`${path}input.txt`, "utf-8");
-console.log(newTextIn);
+// fs.appendFileSync(`${path}input.txt`, textOut, "utf-8");
+
+// const newTextIn = fs.readFileSync(`${path}input.txt`, "utf-8");
+// console.log(newTextIn);
+
+// nonblocking, ascnchronous:
+
+fs.readFile(`${path}start.txt`, "utf-8", (err, data1) => {
+  fs.readFile(`${path}${data1}.txt`, "utf-8", (err, data2) => {
+    fs.readFile(`${path}append.txt`, "utf-8", (err, data3) => {
+      fs.writeFile(`${path}final.txt`, `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("we did it 😁");
+      });
+    });
+  });
+});
+console.log("try to do asynchronous code");
