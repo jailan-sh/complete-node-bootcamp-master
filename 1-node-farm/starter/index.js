538370fa563ee948159f09647b64d5b82068f84a
jailan-sh <jilan.elsherbiny@gmail.com>
2024-04-23 01:05:45 +0200

    server

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// const path = "1-node-farm/starter/txt/";
+
+// blocking, synchronous:
+
+// const text = fs.readFileSync(`${path}input.txt`, "utf-8");
+// console.log(text);
+
+// const textOut = `this new text to the old one which was avocado ${text}. \n created on ${Date.now()}.`;
+// fs.writeFileSync(`${path}output.txt`, textOut);
+
+// fs.appendFileSync(`${path}input.txt`, textOut, "utf-8");
+
+// const newTextIn = fs.readFileSync(`${path}input.txt`, "utf-8");
+// console.log(newTextIn);
+
+// nonblocking, ascnchronous:
+
+// fs.readFile(`${path}start.txt`, "utf-8", (err, data1) => {
+//   fs.readFile(`${path}${data1}.txt`, "utf-8", (err, data2) => {
+//     fs.readFile(`${path}append.txt`, "utf-8", (err, data3) => {
+//       fs.writeFile(`${path}final.txt`, `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("we did it 😁");
+//       });
+//     });
+//   });
+// });
+// console.log("try to do asynchronous code");
+///////////////////////////////////////////////////////////////////////////////////
+
+//serve
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.end("this the server");
+});
+
+server.listen(8000, "127.0.0.1");
