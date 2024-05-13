3d26b7bbcb2c6b78ce2ff07eedd3065f6ccbd988
jailan-sh <jilan.elsherbiny@gmail.com>
2024-05-14 00:20:44 +0300

    caching

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -2,28 +2,28 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
-  // 1
+  1
 
-  //   const readable = fs.readFile("test-file.txt", (err, data) => {
-  //     if (err) console.log(err);
+    const readable = fs.readFile("test-file.txt", (err, data) => {
+      if (err) console.log(err);
 
-  //     res.end(data);
-  //   });
+      res.end(data);
+    });
 
-  //2
+  2
 
-  //   const readable = fs.createReadStream("test-file.txt");
-  //   readable.on("data", (chunk) => {
-  //     res.write(chunk);
-  //   });
-  //   readable.on("end", () => res.end());
-  // readble.0n('error', err => {
-  //     console.log(err);
-  //     res.statusCode = 500;
-  //     res.end('file not found');
-  // })
-  //3
-  // handle problem of backpressure;
+    const readable = fs.createReadStream("test-file.txt");
+    readable.on("data", (chunk) => {
+      res.write(chunk);
+    });
+    readable.on("end", () => res.end());
+  readble.0n('error', err => {
+      console.log(err);
+      res.statusCode = 500;
+      res.end('file not found');
+  })
+  3
+  handle problem of backpressure;
 
   const readable = fs.createReadStream("test-file.txt");
   readable.pipe(res);
