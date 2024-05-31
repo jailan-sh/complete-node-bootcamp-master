bee7383f43d52d29fd0e666b08d8f104baa59224
jailan-sh <jilan.elsherbiny@gmail.com>
2024-05-31 15:14:40 +0300

    up

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -2,17 +2,15 @@ const fs = require('fs');
 const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  if (err) return console.log(err.message);
   console.log(` this: ${data}`);
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, resp) => {
-      if (err) return console.log(err.message);
-      console.log(resp.body.message);
+    .then((res) => {
+      console.log(res.body.message);
 
-      fs.writeFile('dog_pic.txt', resp.body.message, (err) => {
-        if (err) return console.log(err.message);
+      fs.writeFile('dog_pic.txt', res.body.message, (err) => {
         console.log('done with picture');
       });
-    });
+    })
+    .catch((err) => console.log(err.message));
 });
