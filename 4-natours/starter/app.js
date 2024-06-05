73d7281c1ff50be1a9430a50d0220596a39cd43c
jailan-sh <jilan.elsherbiny@gmail.com>
2024-06-05 13:30:16 +0300

    get, post, middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,15 +1,46 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'hello from node.js express', app: 'natours' });
+//middleware:
+app.use(express.json());
+
+// top level code needed data
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+// handling get request:
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('this is a post endpoint......');
+// post method:
+app.post(`/api/v1/tours`, (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  //res.send('done');
 });
 
 const port = 3000;
